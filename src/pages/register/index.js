3df631f0a9b39fab6a380948c79cfd6dc2c8fb2d
goodgamehjli@gmail.com <goodgamehjli@gmail.com git config --global user.email goodgamehjli@gmail.com>
2019-04-09 18:12:55 +0800

    添加服务端代码

---
+++ src/pages/register/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.less'
+export default class Login extends React.Component {
+    componentWillMount() {
+        
+    }
+    render() {
+        return (
+            <div>Login页面</div>
+        )
+    }
+}
--- src/server/server.js
+++ src/server/server.js
@@ -1,1 +1,8 @@
-server.js
+let express = require('express')
+let app = express()
+
+app.get('/123', function(req, res){
+    res.send({'name': 'lhj'})
+})
+
+app.listen(8081)
